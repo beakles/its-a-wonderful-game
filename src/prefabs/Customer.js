1f286f6588edc0a87a1afda882a80c08b2b41140
beakles <beakles@ucsc.edu>
2023-06-04 00:58:47 -0700

    Added ability for customers to select ingredients

--- src/prefabs/Customer.js
+++ src/prefabs/Customer.js
@@ -4,11 +4,15 @@ class Customer extends Phaser.GameObjects.Sprite {
 
     scene.add.existing(this);
 
-    this.favoriteIngredients = {};
+    this.favoriteIngredients = [];
     this.withdrawAmount = 0;
   }
 
-  randomizeIngredients() {
-
+  selectFavoriteIngredients(ingredientsArray) {
+    let randomIngredientAmount = Phaser.Math.Between(1, 3);
+    for (let ingredientSelectionCount = 0; ingredientSelectionCount < randomIngredientAmount; ingredientSelectionCount += 1) {
+      let randomIngredient = ingredientsArray[Phaser.Math.Between(0, ingredientsArray.length)];
+      this.favoriteIngredients.push(randomIngredient);
+    }
   }
 }
