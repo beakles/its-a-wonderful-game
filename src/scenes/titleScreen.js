944c8c36bde00e83239645283381d5f521fe7ec5
SamM922 <samuel.maturo@gmail.com>
2023-06-09 23:34:41 -0700

    Scene start bug fix

--- src/scenes/titleScreen.js
+++ src/scenes/titleScreen.js
@@ -8,12 +8,12 @@ class titleScreen extends Phaser.Scene {
     }
   
     create() {
+        const self = this;
         this.titleScreen = this.add.tileSprite(0, 0, gameConfiguration.width, gameConfiguration.height, 'title').setOrigin(0, 0);
         this.playButton = this.add.rectangle(330, 450, 475, 250, 0x808080).setOrigin(0, 0);
         this.playButton.alpha = 0.5;
         this.playButton.setInteractive();
         this.playButton.on('pointerdown', function(pointer) {
-            console.log('click');
             self.scene.start('iceCreamScene');
         });
     }
